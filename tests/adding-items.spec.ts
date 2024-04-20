bd100869910532d76ca68f3b3fe2a803249e9cf5
Ash <ashwinirao.karai@gmail.com>
2024-04-19 23:13:08 -0600

    annotation: apply describe block and before hook refactor pattern to 'adding multiple todo items' test

--- tests/adding-items.spec.ts
+++ tests/adding-items.spec.ts
@@ -50,17 +50,34 @@ test.describe("when adding a single todo item",
     }
 );
 
-test("should be able to add multiple todo items",
-    async( {page} ) => {
-        await newTodoField.fill("feed the dog");
-        await newTodoField.press("Enter");
 
-        await newTodoField.fill("snuggle with the cat");
-        await newTodoField.press("Enter");
+test.describe("when adding multiple todo items", 
+    async()=>{
+        test.beforeEach("add multiple todo items",
+            async({page}) => {
+                await newTodoField.fill("feed the dog");
+                await newTodoField.press("Enter");
 
-        await expect(displayedTodoItems).toHaveCount(2);
-        await expect(displayedTodoItems).toHaveText(["feed the dog", "snuggle with the cat"]);
+                await newTodoField.fill("snuggle with the cat");
+                await newTodoField.press("Enter");
+            }
+        )
+        test("should be shown all the added items",
+            async( {page} ) => {
+                await expect(displayedTodoItems).toHaveText(["feed the dog", "snuggle with the cat"]);
 
-        await page.close();
+                await page.close();
+            }
+        );
+
+        test("should be shown the count of remaining items",
+            async( {page} ) => {
+                await expect(page.locator(".todo-count"))
+                                    .toHaveText("2 items left");
+
+                await page.close();
+            }
+        );
     }
 );
+
