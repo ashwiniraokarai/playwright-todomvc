90d02e1d66d8d2c32109878b47c3548ef1504a5b
Ash <ashwinirao.karai@gmail.com>
2024-05-08 18:48:37 -0600

    apis: randomize the dummy data via faker

--- tests/intercepting-api-calls.spec.ts
+++ tests/intercepting-api-calls.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test"
 import importedArticlesObject from "./test-data/articles";
+import importedRandomDataArticlesObject from "./test-data/articles-with-randomized-data";
 import importedJSONFileParserObject from "./test-data/JSONFileParser";
 
 test("can intercept and modify response with your own test data (dummy/ mock data)", 
@@ -11,7 +12,8 @@ test("can intercept and modify response with your own test data (dummy/ mock dat
                 console.log(importedArticlesObject.articlesData());
                 await route.fulfill({
                     //convert the JS Object that contains the test data to JSON
-                    body: JSON.stringify(importedArticlesObject.articlesData()),
+                    //body: JSON.stringify(importedArticlesObject.articlesData()),
+                    body: JSON.stringify(importedRandomDataArticlesObject.fakerGeneratedArticlesData()),
                 });
             }
         );
@@ -51,3 +53,22 @@ test("can modify response data (via body), with data extracted from json file",
         await page.goto("https://demo.realworld.io/#/");
     }
 );
+
+
+test("can modify responses data (via body) with randomized data extracted from json file",
+    async({page})=>{
+        const endPointToIntercept = "https://conduit.productionready.io/api/articles?limit=10&offset=0";
+        
+        await page.route(endPointToIntercept, 
+            async(route, request) => {
+                console.log(`Intercepting route: ${request.url()} and sending dummy response body`);
+                
+                await route.fulfill({
+                    body: JSON.stringify(importedRandomDataArticlesObject.fakerGeneratedArticlesData()),
+                })
+            }
+        )
+
+        await page.goto("https://demo.realworld.io/#/");
+    }
+)
---
+++ tests/test-data/articles-with-randomized-data.ts
@@ -0,0 +1,69 @@
+import { faker } from "@faker-js/faker"
+
+/*
+This function returns a JS/TS Object (It's not JSON. Clue: The Keys aren't Strings)
+The resulting JS object is intended to be converted to JSON by the caller before use
+*/
+
+function fakerGeneratedArticlesData(): Object{
+    return {
+        articles:[
+            {
+                slug: faker.lorem.slug(),
+                title: faker.lorem.sentence(),
+                description: faker.lorem.paragraph(),
+                body: faker.lorem.paragraph(),
+                createdAt: faker.date.recent(),
+                updatedAt: "2024-01-04T00:52:58.601Z",
+                favorited: faker.datatype.boolean(),
+                favoritesCount: faker.number.int({max: 999}),
+                author: {
+                  username: faker.person.fullName(),
+                  bio: null,
+                  image: faker.image.url(),
+                  following: faker.datatype.boolean()
+                }
+            },
+            {
+                slug: faker.lorem.slug(),
+                title: faker.lorem.sentence(),
+                description: faker.lorem.paragraph(),
+                body: faker.lorem.paragraph(),
+                createdAt: faker.date.recent(),
+                updatedAt: "2024-01-04T00:52:58.601Z",
+                favorited: faker.datatype.boolean(),
+                favoritesCount: faker.number.int({max: 999}),
+                author: {
+                  username: faker.person.fullName(),
+                  bio: null,
+                  image: faker.image.url(),
+                  following: faker.datatype.boolean()
+                }
+            },
+            {
+                slug: faker.lorem.slug(),
+                title: faker.lorem.sentence(),
+                description: faker.lorem.paragraph(),
+                body: faker.lorem.paragraph(),
+                createdAt: faker.date.recent(),
+                updatedAt: "2024-01-04T00:52:58.601Z",
+                favorited: faker.datatype.boolean(),
+                favoritesCount: faker.number.int({max: 999}),
+                author: {
+                  username: faker.person.fullName(),
+                  bio: null,
+                  image: faker.image.url(),
+                  following: faker.datatype.boolean()
+                }
+            }
+        ]
+    }
+}
+
+/*
+ A note about Exports:
+ Alternately you could export just the function => aka "named" export like this: export jsonData
+ Exporting as a "default" on the other hand, allows me to wrap up the function as an object
+ Plus, the importer does not have to worry about the specifics to import. And can look up callable elements using the default exported object
+*/
+export default {fakerGeneratedArticlesData};
